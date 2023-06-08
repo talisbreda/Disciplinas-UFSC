5a8698b5710c4de03ef85f33bfd1d5cd6b905d4c
talisbreda <talis.breda@outlook.com>
2023-06-08 15:19:21 -0300

    Continuacao do VPL de arvores binarias de ED

--- INE5408-Estruturas-de-Dados/arvore_binaria/binary_tree.h
+++ INE5408-Estruturas-de-Dados/arvore_binaria/binary_tree.h
@@ -6,25 +6,37 @@ template<typename T>
 class BinaryTree {
 public:
     ~BinaryTree() {
-        root = nullptr;
         size_ = 0;
+        root = nullptr;
     }
 
     void insert(const T& data) {
-        if (!root) {
+        if (empty()) {
             root = new Node(data);
         } else {
             root->insert(data);
         }
+        size++;
     }
 
-    void remove(const T& data);
+    void remove(const T& data) {
+        if (empty()) {
+            throw std::out_of_range("Árvore vazia")
+        } else {
+            root->remove(data);
+            size--;
+        }
+    }
 
     bool contains(const T& data) const;
 
-    bool empty() const;
+    bool empty() const {
+        return size_ == 0;
+    }
 
-    std::size_t size() const;
+    std::size_t size() const {
+        return size;
+    }
 
     ArrayList<T> pre_order() const;
 
@@ -34,8 +46,10 @@ public:
 
 private:
     struct Node {
-        Node(const T& data):
-            data{data}
+        explicit Node(const T& data):
+            this->data{data},
+            left{nullptr},
+            right{nullptr}
         {}
 
         T data;
@@ -43,22 +57,95 @@ private:
         Node* right;
 
         void insert(const T& data_) {
-            if (data_ > data) {
-                if (!right) {
-                    right = new Node(data);
+            if (data_ > this->data) {
+                if (this->right == nullptr) {
+                    this->final_insert(data_);
                 } else {
-                    right->insert(data);
+                    this->right->insert(data_);
                 }
             } else {
-                if (!left) {
-                    left = new Node(data);
+                if (this->left == nullptr) {
+                    this->final_insert(data_);
                 } else {
-                    left->insert(data);
+                    this->left->insert(data_);
                 }
             }
         }
 
-        bool remove(const T& data_);
+        void final_insert(const T& data_) {
+            Node* newNode = new Node(data_);
+            if (data_ > this->data) {
+                this->right = newNode;
+            } else {
+                this->left = newNode;
+            }
+        }
+
+        bool remove(const T& data_) {
+            if (data_ == this->right->data) {
+                this->right->remove_right(data, this);
+            } else if (data_ == this->left->data) {
+                this->left->remove_left(data, this);
+            } else if (data_ > this->data) {
+                this->right->remove(data_);
+            } else if (data_ < this->data) {
+                this->left->remove(data_);
+            } else if (data_ == this->data) {
+                if (this->left == nullptr && this->right == nullptr) {
+                    delete this;
+                } else {
+                    remove_2_3();
+                }
+            }
+        }
+
+        void remove_right(const T& data_, Node* pai) {
+            if (this->left == nullptr && this->right == nullptr) {
+                remove_case_1_right(pai);
+            } else {
+                remove_2_3(pai);
+            }
+        }
+
+        void remove_left(const T& data_, Node* pai) {
+            if (this->left == nullptr && this->right == nullptr) {
+                remove_case_1_left(pai);
+            } else {
+                remove_2_3(pai);
+            }
+        }
+
+        void remove_case_1_left(Node* pai) {
+            pai->left = nullptr;
+            delete this;
+        }
+
+        void remove_case_1_right(Node* pai) {
+            pai->right = nullptr;
+            delete this;
+        }
+
+        void remove_2_3(Node* pai) {
+            if (this->left != nullptr && this->right != nullptr) {
+                remove_case_3();
+            } else {
+                remove_case_2(pai);
+            }
+        }
+
+        void remove_case_2(Node* pai) {
+            if (this->left != nullptr) {
+                pai->right = this->left;
+            } else {
+                pai->right = this->right;
+            }
+        }
+
+        void remove_case_3() {
+            Node* min = minimum(this->right);
+            this->data = min->data;
+            min->remove(this->data);
+        }
 
         bool contains(const T& data_) const;
 
@@ -69,12 +156,27 @@ private:
         void post_order(ArrayList<T>& v) const;
     };
 
-    Node* travel(T& data) {
-
-    }
-
     Node* root;
     std::size_t size_;
+
+    Node* find(const T& data) {
+        Node* aux = root;
+        while (aux->data != data || aux != nullptr) {
+            if (data > aux->data) {
+                aux = aux->right;
+            } else {
+                aux = aux->left;
+            }
+        }
+        return aux;
+    }
+
+    Node* minimum(Node* sub) {
+        while (sub->left != nullptr) {
+            sub = sub->left;
+        }
+        return sub;
+    }
 };
 
 }
