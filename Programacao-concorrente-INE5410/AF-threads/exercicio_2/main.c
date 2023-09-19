2fa30da42d7da260f4972099715709ad0df96c73
Tális Breda <talisbreda@gmail.com>
2023-09-19 16:04:40 -0300

    Finalizada AF2.2 (threads)

--- Programacao-concorrente-INE5410/AF-threads/exercicio_2/main.c
+++ Programacao-concorrente-INE5410/AF-threads/exercicio_2/main.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <math.h>
 
 // Lê o conteúdo do arquivo filename e retorna um vetor E o tamanho dele
 // Se filename for da forma "gen:%d", gera um vetor aleatório com %d elementos
@@ -20,38 +21,24 @@ double* load_vector(const char* filename, int* out_size);
 // tenham tamanho size.
 void avaliar(double* a, double* b, double* c, int size);
 
-typedef struct Range {
-    int start;
-    int end;
-    double* v1;
-    double* v2;
-    double* v3;
-} Range;
+typedef struct Info {
+    int startIndex;
+    int endIndex;
+    double* a;
+    double* b;
+    double* c;
+} Info;
 
-typedef struct Distribution {
-    int valor;
-    int indiceTroca;
-} Distribution;
+void* sum(void* var_struct) {
+    struct Info* info = (struct Info*) var_struct;
 
-void* worker(void* arg) {
-    Range* range = (Range*)arg;
-    for (int i = range->start; i < range->end; i++) {
-        range->v3[i] = range->v1[i] + range->v2[i];
+    for(int i = info->startIndex; i < info->endIndex; i++) {
+        info->c[i] = info->a[i] + info->b[i];
     }
-    return NULL;
+
+    pthread_exit(NULL);
 }
 
-Distribution distributeThreads(int n_threads, int a_size) {
-    int resto = n_threads % a_size;
-    Distribution d;
-    d.valor = n_threads / a_size;
-    if (resto == 0) {
-        d.indiceTroca = -1;
-        return d;
-    }
-    d.indiceTroca = a_size - resto;
-    return d;
-}
 
 int main(int argc, char* argv[]) {
     // Gera um resultado diferente a cada execução do programa
@@ -72,7 +59,7 @@ int main(int argc, char* argv[]) {
   
     //Quantas threads?
     int n_threads = atoi(argv[1]);
-    if (!n_threads) {
+    if (n_threads <= 0) {
         printf("Número de threads deve ser > 0\n");
         return 1;
     }
@@ -84,6 +71,7 @@ int main(int argc, char* argv[]) {
         printf("Erro ao ler arquivo %s\n", argv[2]);
         return 1;
     }
+
     double* b = load_vector(argv[3], &b_size);
     if (!b) {
         printf("Erro ao ler arquivo %s\n", argv[3]);
@@ -101,30 +89,41 @@ int main(int argc, char* argv[]) {
     // Calcula com uma thread só. Programador original só deixou a leitura 
     // do argumento e fugiu pro caribe. É essa computação que você precisa 
     // paralelizar
-
-    pthread_t threads[n_threads];
-    Range* ranges[n_threads];
-    Distribution d = distributeThreads(n_threads, a_size);
-    for (int i = 0; i < n_threads; i++) {
-        Range* range = (Range*)malloc(sizeof(Range));
-        if (d.indiceTroca == i) {
-            d.valor++;
-        }
-        range->start = i * d.valor;
-        range->end = (i + 1) * d.valor;
-        range->v1 = a;
-        range->v2 = b;
-        range->v3 = c;
-        ranges[i] = range;
-        pthread_create(&threads[i], NULL, worker, (void*)range);
-    }
-    for (int i = 0; i < n_threads; i++) {
-        pthread_join(threads[i], NULL);
-        free(ranges[i]);
-    }
-
     // for (int i = 0; i < a_size; ++i) 
     //     c[i] = a[i] + b[i];
+
+    if (n_threads > a_size) {
+        n_threads = a_size;
+    }
+
+    pthread_t threads[n_threads];
+        struct Info* info;
+
+    double action = (a_size + n_threads - 1) / n_threads;
+    double elementsToPass = ceil(action);
+    Info* infos[n_threads];
+    for (int i = 0; i < n_threads; i++) {
+        info = malloc(sizeof(struct Info));
+        infos[i] = info;
+        info->startIndex = elementsToPass * i;
+        info->endIndex = elementsToPass * (i + 1);
+
+        if (elementsToPass * (i + 1) > a_size) {
+            info->endIndex = a_size;
+        }
+
+        info->a = a;
+        info->b = b;
+        info->c = c;
+
+        pthread_create(&threads[i], NULL, sum, (void*) info);
+    }
+
+    for (int i = 0; i < n_threads; i++) {
+        pthread_join(threads[i], NULL);
+        free(infos[i]);
+    }
+
 
     //    +---------------------------------+
     // ** | IMPORTANTE: avalia o resultado! | **
